88f87cfb5cf10467177804e61cbf42c8b6b039c9
billkiplagat <bill.kiplagat.7@gmail.com>
2023-09-11 13:38:46 +0300

    Updated rules

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-if (process.argv && process.argv.length > 2)
-{
-    console.log(process.argv[2]);
+if (process.argv && process.argv.length > 2) {
+  console.log(process.argv[2]);
 } else {
-    console.log("No argument");
+  console.log('No argument');
 }
--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-const words = ["C is fun", "Python is cool", "JavaScript is amazing"]
-for (const i of words)
-{
-    console.log(i);
+const words = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const i of words) {
+  console.log(i);
 }
